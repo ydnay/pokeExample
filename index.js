5383b6cc77164376dd11d7c94436b85ce3ada718
Yandy Gonzalez <yandygonzalez@gmail.com>
2018-06-25 20:05:15 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+// index.js
+
+const pokeApi = axios.create({
+  baseURL: 'https://pokeapi.co/api/v2/pokemon'
+})
+
+function getPokemonInfo(id) {
+pokeApi.get(id)
+.then(response => {
+  console.log(response.data)
+})
+.catch(err => {
+  console.error(err)
+})
+}
+
+document.getElementById("pokeButton").onclick = function(){
+getPokemonInfo("1");
+}
